3a3d5271c39244a1047d50030de8de5df016f41c
Ricar <ricardo_saikali@hotmail.com>
2021-06-11 18:07:11 -0400

    not working

--- SEG1525-Lab5/scripts/myJS.js
+++ SEG1525-Lab5/scripts/myJS.js
@@ -1,7 +1,28 @@
+var anytime = [];
+var ricardotime = [1,2];
+var johntime = [3,4];
+var thomastime = [5,6];
+var currentDayFilter=[];
 
-
+var maptime = {
+    "Any": anytime,
+    "Dr. Ricardo": ricardotime,
+    "Dr. John": johntime,
+    "Dr. Thomas": thomastime
+};
 
 $(document).ready(function(){
+    var myform = $("#bookform");
+    var firstname = document.querySelector("#firstName");
+    var lastName = $("#lastName");
+    var email = $("#email");
+    var phone = $("#phonenumber");
+    var selectPro = $("#selectPro");
+    var calendar = $("#datetimepicker");
+    var card = $("#card");
+    var inputBooking = $(".inputBooking");
+    var output = document.querySelector("output");
+
     $(document).on("click", ".open-bookAppointment", function (e) {
         e.preventDefault();
         var serviceName = $(this).data('id')+" Booking";
@@ -9,6 +30,32 @@ $(document).ready(function(){
     });
 
 
+    // $('#datetimepicker').datetimepicker();
+    $("#datetimepicker").datetimepicker({
+        // value:'2021-06-11',
+        disabledWeekDays: currentDayFilter
+    });
+
+    $('#toggle').on("click", function(){
+        $('#datetimepicker').datetimepicker("toggle");
+    });
+
+    $("#selectPro").on("change", function(){
+        // $('#datetimepicker').datetimepicker();
+        $('#datetimepicker').datetimepicker("destroy");
+        datefunc();
+        
+        $('#datetimepicker').datetimepicker("testing");      
+    });
+
+
+
+    function datefunc(){
+        let days = [];
+        days.push(maptime[ selectPro.children("option:selected").text()]);
+        currentDayFilter = days;
+        return days;
+    }   
 
 
 
