be86d72f899fba6283c8fa1d62be25c4ed453369
Ricar <ricardo_saikali@hotmail.com>
2021-06-23 14:29:25 -0400

    lab6 from lab5

---
+++ SEG1525-Lab6/scripts/myJS.js
@@ -0,0 +1,100 @@
+var anytime = [6,0];
+var ricardotime = [6,0,1,2];
+var johntime = [6,0,3,4];
+var thomastime = [6,0,5,3];
+let currentDayFilter;
+
+var maptime = {
+    "Any - Weekdays Only": anytime,
+    "Dr. Ricardo - Wed, Thu, Fri Only": ricardotime,
+    "Dr. John - Mon, Tue, Fri Only": johntime,
+    "Dr. Thomas - Mon, Tue, Thu Only": thomastime
+};
+
+
+
+
+$(document).ready(function(){
+    var myform = $("#bookform");
+    var firstname = document.querySelector("#firstName");
+    var lastName = document.querySelector("#lastName");
+    var email = $("#email");
+    var phone = $("#phonenumber");
+    var selectPro = $("#selectPro");
+    var calendar = $("#datetimepicker");
+    var card = $("#card");
+    var inputBooking = $(".inputBooking");
+    var output = document.querySelector("output");
+
+    $(document).on("click", ".open-bookAppointment", function (e) {
+        e.preventDefault();
+        var serviceName = $(this).data('id')+" Booking";
+        $("#modalTitle").text( serviceName );
+    });
+
+    datefunc();
+
+    $("#datetimepicker").datetimepicker({
+        minDate:0,
+        disabledWeekDays: currentDayFilter
+    });
+    
+    $('#toggle').on("click", function(){
+        $('#datetimepicker').datetimepicker("show");
+    });
+
+    $("#selectPro").on("change", function(){
+        
+        $('#datetimepicker').datetimepicker("destroy");
+        datefunc();
+        var container = document.getElementById("holdCalendar");
+        var content = container.innerHTML;
+        container.innerHTML= " "; 
+        container.innerHTML= content; 
+        $("#datetimepicker").datetimepicker({
+            // value:'2021-06-11',
+            minDate:0,
+            disabledWeekDays: currentDayFilter
+        });
+        $('#toggle').on("click", function(){
+            $('#datetimepicker').datetimepicker("show");
+        });  
+    });
+
+    
+    function datefunc(){
+        currentDayFilter = maptime[ selectPro.children("option:selected").text()];
+        return currentDayFilter;
+    };
+
+    
+    document.getElementById("bookform").onsubmit = submitfunc;
+
+    function submitfunc() {
+        var selectPro = $("#selectPro");
+        var doctor = selectPro.children("option:selected").val();
+        var calendar = $("#datetimepicker");
+        alert("Your appointment has been booked successfully.\n Doctor: " + doctor+ "\n Day and time: "+ calendar.val());
+    };
+
+
+
+    
+
+
+    // firstname.oninvalid = function(event) {
+    //     event.target.setCustomValidity('Please make sure this field is written correctly and does not have any special characters other than a - or \'');
+    // };
+
+    // lastName.oninvalid = function(event) {
+    //     event.target.setCustomValidity('Please make sure this field is written correctly and does not have any special characters other than a - or \'');
+    // };
+    // firstname.oninput = function(event) {
+    //     event.target.setCustomValidity(' ');
+    // }
+    // lastName.oninput = function(event) {
+    //     event.target.setCustomValidity(' ');
+    // }
+
+
+});
